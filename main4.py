74bea5903eaa71db754473e61c691d9ef0225357
Anas lachhab <anaslachhab666@gmail.com>
2026-01-17 16:40:23 +0100

    Update main4.py

--- main4.py
+++ main4.py
@@ -34,7 +34,7 @@ if RESULT_COLUMN not in df.columns:
 # SCRAPING LOOP
 # -----------------------------
 #for index, row in df.head(1000).iterrows():
-for index, row in df.iloc[3000:4000].iterrows():
+for index, row in df.iloc[4000:5000].iterrows():
 
     if pd.notna(row[RESULT_COLUMN]):
         continue  # already processed
